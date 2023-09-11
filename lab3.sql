201072cd73c8e9865d1cae4a4d8ee53e98718f26
swoot10 <122939325+swoot10@users.noreply.github.com>
2023-09-11 17:59:56 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,6 +1,6 @@
 --q2.1
 insert into student(s_email,s_name,major)
 values('s1@jmu.edu','s1','IA'),
-		('s2@jmu.edu','s1','IA'),
-		('s3@jmu.edu','s1','ISAT'),
-		('s4@jmu.edu','s1','ISAT')
+		('s2@jmu.edu','s2','IA'),
+		('s3@jmu.edu','s3','ISAT'),
+		('s4@jmu.edu','s4','ISAT')

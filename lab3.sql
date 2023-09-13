2f9da7bbeeadbb79455bdac0c85a8f4eac2965aa
swoot10 <122939325+swoot10@users.noreply.github.com>
2023-09-13 17:13:07 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -4,3 +4,7 @@ values('s1@jmu.edu','s1','IA'),
 		('s2@jmu.edu','s2','IA'),
 		('s3@jmu.edu','s3','ISAT'),
 		('s4@jmu.edu','s4','ISAT')
+--q2.2
+insert into professor(p_email,p_name,office)
+values			('p1@jmu.edu','p1','o1'),
+				('p2@jmu.edu','p2','o2')

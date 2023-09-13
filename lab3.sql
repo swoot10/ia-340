8f63a2694da3d50baf38e9d6aaf499326d89baed
swoot10 <122939325+swoot10@users.noreply.github.com>
2023-09-13 17:22:20 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -8,3 +8,8 @@ values('s1@jmu.edu','s1','IA'),
 insert into professor(p_email,p_name,office)
 values			('p1@jmu.edu','p1','o1'),
 				('p2@jmu.edu','p2','o2')
+--q2.3
+insert into course(c_number,c_name,room,p_email)
+values      ('c1','postgresql','r1','p1@jmu.edu'),
+			('c2','mongodb','r2','p2@jmu.edu'),
+			('c3','twitter','r1','p1@jmu.edu')

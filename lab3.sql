3e1fc7ae988ea12655d529dc90253f685a13561f
swoot10 <122939325+swoot10@users.noreply.github.com>
2023-09-20 17:44:00 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -55,3 +55,13 @@ select professor.p_name,
 from professor
 inner join course
 on professor.p_email = course.p_email
+
+--q2.10
+select professor.p_name,
+		count (course.c_name) as num_course
+from professor
+inner join course
+on professor.p_email = course.p_email
+group by professor.p_name
+order by num_course desc
+limit 1
